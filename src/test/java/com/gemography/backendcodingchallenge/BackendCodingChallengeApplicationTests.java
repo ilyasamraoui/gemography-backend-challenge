2a3bcf7012c058e418cc4bcc082a2fde42608bb4
ilyasamraoui <ilyasamraoui1@gmail.com>
2020-11-19 13:46:44 +0100

    first-commit

---
+++ src/test/java/com/gemography/backendcodingchallenge/BackendCodingChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gemography.backendcodingchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendCodingChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
