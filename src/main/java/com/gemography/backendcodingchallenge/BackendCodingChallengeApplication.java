34dfe200d28c14da2ce981fa22c6b26686d4b8bb
ilyasamraoui <ilyasamraoui1@gmail.com>
2020-11-19 15:42:09 +0100

    adding language resource to get the trending languages

--- src/main/java/com/gemography/backendcodingchallenge/BackendCodingChallengeApplication.java
+++ src/main/java/com/gemography/backendcodingchallenge/BackendCodingChallengeApplication.java
@@ -2,8 +2,10 @@ package com.gemography.backendcodingchallenge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class BackendCodingChallengeApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/gemography/backendcodingchallenge/web/rest/LanguageResource.java
+++ src/main/java/com/gemography/backendcodingchallenge/web/rest/LanguageResource.java
@@ -1,4 +1,36 @@
 package com.gemography.backendcodingchallenge.web.rest;
 
+import com.gemography.backendcodingchallenge.domain.Language;
+import com.gemography.backendcodingchallenge.domain.RepositorySearchResult;
+import com.gemography.backendcodingchallenge.service.LanguageService;
+import com.gemography.backendcodingchallenge.web.rest.client.GitHubApiProxy;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.util.Set;
+
+/**
+ * REST controller for managing {@link Language}.
+ */
+@RestController
 public class LanguageResource {
+    private final GitHubApiProxy githubApiProxy;
+    private final LanguageService languageService;
+
+    public LanguageResource(GitHubApiProxy githubApiProxy, LanguageService languageService) {
+        this.githubApiProxy = githubApiProxy;
+        this.languageService = languageService;
+    }
+
+    /**
+     * {@code GET  /trendingLanguages} : get languages used by the 100 trending public repos on GitHub.
+     *
+     * @return the {@link Set<Language>} with status {@code 200 (OK)}.
+     */
+    @GetMapping("/trendingLanguages")
+    public Set<Language> trendingLanguages() {
+        RepositorySearchResult result = githubApiProxy.fetchTrendingRepos(LocalDate.now().minusDays(30).toString());
+        return languageService.getTrendingLanguages(result);
+    }
 }
